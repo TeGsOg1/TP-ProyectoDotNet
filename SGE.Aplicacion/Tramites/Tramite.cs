f6667a7b95953138cec66ff8599365306b8470b5
valentin <valentinlollagalvan@hotmail.com>
2026-05-14 23:42:44 -0300

    tramites por la mitad

--- SGE.Aplicacion/Tramites/Tramite.cs
+++ SGE.Aplicacion/Tramites/Tramite.cs
@@ -1,5 +1,11 @@
 namespace SGE.Aplicacion.Tramites;
-class Tramite
+    public class Tramite
 {
-    
+    public Guid Id { get; private set; }
+    public Guid ExpedienteId { get; private set; }
+    public ContenidoTramite Contenido { get; private set; }
+    public EtiquetaTramite Etiqueta { get; private set; }
+    public DateTime FechaCreacion { get; private set;}
+    public DateTime FechaUltimaModificacion {get; private set;}
+    public Guid IdUsuarioUltimoCambio { get; private set; }
 }
