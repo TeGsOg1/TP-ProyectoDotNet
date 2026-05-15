26c6616544bd349c40338cc14f271ff0d032d1ee
valentin <valentinlollagalvan@hotmail.com>
2026-05-14 22:34:42 -0300

    slx

--- SGE.Aplicacion/Tramites/ITramiteRepository.cs
+++ SGE.Aplicacion/Tramites/ITramiteRepository.cs
@@ -2,9 +2,9 @@ namespace SGE.Aplicacion.Tramites;
 public interface ITramiteRepository
 {
     void AgregarTramite (Tramite tramite);
-    Tramites? ObtenerTramiteId (Guid id);
+    Tramite? ObtenerTramiteId (Guid id);
     //IEnumerable<Tramite> ObtenerPorExpeienteId(Guid expedienteId);
-    void ModificarTramite(Tramites t);
+    void ModificarTramite(Tramite t);
     void EliminarTramite(Guid Id);
     void EliminarPorExpedienteI (Guid expedienteId);
 }
