3f2cc2fcf709580ca4aff22760ca420344ae1430
dantepoblete <dantepoblete@outlook.com>
2026-06-28 22:26:41 -0300

    archivos

---
+++ SGE.Infraestructura/BaseSqlite.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SGE.Repositorios;
+
+public class BaseSqlite
+{
+    public static void Inicializar()
+    {
+        using var context = new BaseContext();
+        context.Database.EnsureCreated();
+        var connection = context.Database.GetDbConnection();
+        connection.Open();
+        using var command = connection.CreateCommand();
+        command.CommandText = "PRAGMA journal_mode=DELETE;";
+        command.ExecuteNonQuery();
+    }
+}
---
+++ SGE.Infraestructura/RepositorioExpediente.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGE.Aplicacion;
+namespace SGE.Repositorios;
+
+public class RepositorioExpediente : IExpedienteRepositorio
+{
+    public void AltaExpediente(Expediente e)
+    {
+        using var context = new BaseContext();
+        context.Expedientes.Add(e);
+        context.SaveChanges();
+    }
+
+    public void BajaExpediente(int id)
+    {
+        using var context = new BaseContext();
+        var e = context.Expedientes.Where(t => t.Id == id).SingleOrDefault();
+        if (e != null)
+        {
+            context.Expedientes.Remove(e);
+        }
+        context.SaveChanges();
+    }
+
+    public void ModificarExpediente(Expediente nuevoE)
+    {
+        using var context = new BaseContext();
+        var e = context.Expedientes.Where(e => e.Id == nuevoE.Id).SingleOrDefault();
+        if (e != null)
+        {
+            e.Caratula = nuevoE.Caratula;
+            e.Estado = nuevoE.Estado;
+        }
+        context.SaveChanges();
+    }
+
+    public Expediente? ConsultaPorId(int id)
+    {
+        using var context = new BaseContext();
+        return context.Expedientes.Where(e => e.Id == id).SingleOrDefault();
+    }
+
+    public List<object> ConsultaTodos(int id)
+    {
+        using var context = new BaseContext();
+        List<object> lista = [];
+        var exp = context.Expedientes.Where(e => e.Id == id).SingleOrDefault();
+        if (exp != null)
+        {
+            lista.Add(exp);
+            var tramites = context.Tramites.Where(t => t.ExpedienteId == id).ToList();
+            lista.AddRange(tramites);
+        }
+        return lista;
+    }
+
+    public List<Expediente> ListarExpedientes()
+    {
+        using var context = new BaseContext();
+        return context.Expedientes.ToList();
+    }
+}
