95d2e43abf22a4364592c44cda4b6e9769e12d43
Santiago Mordeglia <santimor2003@gmail.com>
2026-07-14 16:35:00 -0300

    modificacion de program.cs para ejecucion de la api

--- SGE.WebAPI/Program.cs
+++ SGE.WebAPI/Program.cs
@@ -4,6 +4,8 @@ using SGE.WebAPI.Middlewares;
 using Scalar.AspNetCore;
 using SGE.Aplicacion; 
 using SGE.Infraestructura.Extensiones;
+using SGE.Infraestructura.Datos; 
+using SGE.Aplicacion.Usuarios; 
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,14 @@ builder.Services.AddOpenApi()
     .AddExceptionHandler<ExcepcionGlobalMiddleware>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<SgeContext>();
+    var hashService = scope.ServiceProvider.GetRequiredService<IHashContrasenaService>(); 
+
+    SgeSqlite.Inicializar(context, hashService);
+}
 
 if (app.Environment.IsDevelopment())
 {
